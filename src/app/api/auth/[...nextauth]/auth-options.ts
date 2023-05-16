4f8b694f80f12befe8a371c85e9ae2816d2a9d07
Jackson Garinger <jackson.garinger@gmail.com>
2023-05-15 20:33:59 -0500

    Hopefully fixed weird type error

---
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -0,0 +1,12 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
